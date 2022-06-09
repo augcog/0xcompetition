587750532f7939f4446ca082e86df2daf065eeff
winsonchen22 <wchen453@wisc.edu>
2022-06-09 12:57:56 -0700

    Starting full version

--- EasyVersion/competition_sql_wrapper.py
+++ EasyVersion/competition_sql_wrapper.py
@@ -5,18 +5,18 @@ from sqlalchemy import create_engine, inspect
 
 class CompetitionSQLWrapper:
 
-    """
-    Takes a file path, validate whether this file path is correct or not, and connect to the database
-
-    raise file not found error if file path is wrong
-    raise Connection\error if connection failes.
-    Args:
-        file_path:
-
-    Returns:
-
-    """
     def __init__(self, file_path: str):
+        """
+        Takes a file path, validate whether this file path is correct or not, and connect to the database
+
+        raise file not found error if file path is wrong
+        raise Connection\error if connection fails.
+        Args:
+            file_path:
+
+        Returns:
+
+        """
         try:
             with open(file_path) as f:
                 data = json.load(f)
@@ -25,7 +25,6 @@ class CompetitionSQLWrapper:
         except OSError:
             raise OSError("File not found. Check the name of the file.")
 
-        # print(data)
         username = data['username']
         password = data['password']
         ip_address = data['ip_address']
@@ -45,110 +44,102 @@ class CompetitionSQLWrapper:
         except ConnectionError:
             raise ConnectionError("Failed to connect!!!")
 
-    """
-    Using a user SQL statement input, this function will use the database 
-    connection to execute the statement and return a pandas dataframe
-    Args:
-        query:
-
-    Returns:
-        
-    """
     def query(self, query: str) -> pd.DataFrame:
+        """
+        Using a user SQL statement input, this function will use the database
+        connection to execute the statement and return a pandas dataframe
+        Args:
+            query:
 
+        Returns:
+
+        """
         results = pd.read_sql_query(query, self.db_connection)
         # TODO: implement column name parsing
         df = pd.DataFrame(results, columns=None)
 
         return df
 
-    """
-    Creates and prints out a list of tables in the database
-    Args:
-        None
-
-    Returns:
-        tables: a list of the table names in the database
-
-    """
     def list_tables(self):
+        """
+        Creates and prints out a list of tables in the database
+        Args:
+            None
 
-        tables = self.alchemy_engine.table_names()
-        print(tables)
+        Returns:
+            tables: a list of the table names in the database
 
+        """
+        tables = self.inspector.get_table_names()
         return tables
 
-    """
-    Creates and prints out a list of columns in a specific table
-    Args:
-        table_name:
-
-    Returns:
-
-    """
     def list_columns(self, table_name: str):
+        """
+        Creates and prints out a list of columns in a specific table
+        Args:
+            table_name:
 
+        Returns:
+            cols
+
+        """
         try:
             cols = []
             for results in self.inspector.get_columns(table_name):
                 cols.append(results.get('name'))
-            print(cols)
         except NameError:
             raise NameError("Table name, " + table_name + " is not valid!")
 
         return cols
 
-    """
-    Creates and prints out a list of indices in a specific table
-    Args:
-        table_name:
-
-    Returns:
-        cols:
-    """
     def list_indexes(self, table_name: str):
-
-        try:
-            indexes = []
-            for results in self.inspector.get_indexes(table_name):
-                indexes.append(results.get('name'))
-            print(indexes)
-        except NameError:
-            raise NameError("Table name, " + table_name + " is not valid!")
-
-        return indexes
-
-    """
-    Creates and prints out a list of indices in a specific table
-    Args:
-        table_name:
-
-    Returns:
-        cols:
-    """
-    def get_primary_key_info(self, table_name: str):
-
-        try:
-            pk = []
-            for results in self.inspector.get_pk_constraint(table_name):
-                pk.append(results)
-            print(pk)
-        except NameError:
-            raise NameError("Table name, " + table_name + " is not valid!")
-
-        return pk
-
-    """
+        """
         Creates and prints out a list of indices in a specific table
         Args:
             table_name:
 
         Returns:
-            cols:
+            indexes:
+
         """
+        try:
+            indexes = []
+            for results in self.inspector.get_indexes(table_name):
+                indexes.append(results.get('name'))
+        except NameError:
+            raise NameError("Table name, " + table_name + " is not valid!")
+
+        return indexes
+
+    def get_primary_key_info(self, table_name: str):
+        """
+        Creates and prints out a list of indices in a specific table
+        Args:
+            table_name:
+
+        Returns:
+            pk:
+            
+        """
+        try:
+            pk = []
+            for results in self.inspector.get_pk_constraint(table_name):
+                pk.append(results)
+        except NameError:
+            raise NameError("Table name, " + table_name + " is not valid!")
+
+        return pk
 
     def get_foreign_keys_info(self, table_name: str):
+        """
+        Creates and prints out a list of indices in a specific table
+        Args:
+            table_name:
 
+        Returns:
+            fk:
+
+        """
         try:
             fk = []
             for results in self.inspector.get_foreign_keys(table_name):
