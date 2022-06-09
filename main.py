7006dd65f3bc7ef80bc9c5e7e598b7f92597ad51
winsonchen22 <wchen453@wisc.edu>
2022-06-09 12:00:20 -0700

    Starting full version

--- main.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from competition_sql_wrapper import *
-
-if __name__ == "__main__":
-    fp = "./settings.json"
-    competition_sql_wrapper = CompetitionSQLWrapper(file_path=fp)
-
